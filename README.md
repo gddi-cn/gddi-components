be487bc364cb074e692bbad07b2004d2efe87b28
ruobaole <daisyrpanglive@live.com>
2022-05-26 15:32:35 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,15 @@
+# A library holding some of GDDi's 'mysterious' components
+
+- **Demo Page:** ...🚧
+
+## Installation
+
+...🚧
+
+## Simple Usage
+
+...🚧
+
+## Testing
+
+...🚧
---
+++ rollup.config.js
@@ -0,0 +1,43 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
+import postcss from 'rollup-plugin-postcss'
+import { terser } from 'rollup-plugin-terser'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import bundleSize from 'rollup-plugin-bundle-size'
+
+const packageJson = require('./package.json')
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      postcss(),
+      terser(),
+      bundleSize()
+    ],
+    external: ['react', 'react-dom', 'styled-components', 'fabric'],
+  },
+  {
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+  },
+]
